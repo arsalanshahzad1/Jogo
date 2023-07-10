1b77ecb122ea2018bc62ed7258822c6ab179ab66
Muhammadnameer <nameerfarooquit@gmail.com>
2023-07-10 07:25:29 -0700

    popup added

--- frontend/src/pages/landingpage/LandingPage.jsx
+++ frontend/src/pages/landingpage/LandingPage.jsx
@@ -26,7 +26,8 @@ import LinkedIn from "../../components/svg/LinkedIn";
 import { Loader } from "../../assets/Loader/Loader";
 import WhitePaper from "../../components/svg/WhitePaper";
 
-
+import metamask from '../../../public/assets/images/metamask.png'
+import trustwallet from '../../../public/assets/images/trustwallet.png'
 
 
 
@@ -41,7 +42,7 @@ const getAIMTokenContrat = () => {
 }
 
 
-const LandingPage = ({walletConnected,changeNetwork,account,setAccount,state, setState, index, setIndex , loader , setloader }) => {
+const LandingPage = ({ walletConnected, changeNetwork, account, setAccount, state, setState, index, setIndex, loader, setloader }) => {
   const zero = BigNumber.from(0);
   const [roundNumber, setRoundNumber] = useState(zero);
   const web3ModalRef = useRef();
@@ -116,7 +117,7 @@ const LandingPage = ({walletConnected,changeNetwork,account,setAccount,state, se
   // }, [walletConnected]);
 
   useEffect(() => {
-  
+
     window.scrollTo({ top: 0 });
     document
       .getElementById(state)
@@ -147,202 +148,223 @@ const LandingPage = ({walletConnected,changeNetwork,account,setAccount,state, se
 
   // }
 
+
+  const [ShowPopup, setShowPopup] = useState(false)
   return (
     <>
-    {
-   loader ? <Loader/> :
-        
-      <div className="home_wrap">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="sec-wrap">
+      {
+        loader ? <Loader /> :
+
+          <div className="home_wrap">
+            <div className="container-fluid">
               <div className="row">
-                <div className="col-lg-12">
-                  <div className="top-bar">
-                    <div className="sec-left-wrap "></div>
+                <div className="sec-wrap">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div className="top-bar">
+                        <div className="sec-left-wrap "></div>
 
-                    <div className="sec-right-wrap hide-on-mobile">
-                      <div className="left hide">
-                        <span onClick={changeNetwork}>
-                          <Wallet classes={"wallet-btn"} />
-                        </span>
-                      </div>
-                      <div className="right">
-                        <Link >
-                          <span onClick={() =>setState('presale-section')}>
-                            <BuyNow activeSection={activeSection} />
-                          </span>
-                        </Link>
-                        <ul className="social-icons">
-                          <li>                      
-                            <Link onClick={changeNetwork}><Wallet/>
-                            </Link>
-                          </li>
-                          <li>
-                            <a href="https://twitter.com/JogoAimeme" target="_blank"><Twiitter/></a>
-                          </li>
-                          <li>
-                            <a href="https://t.me/JogoAimeme" target="_blank"><Telegram /></a>{" "}
-                          </li>
-                          <li>
-                            <a href="https://www.linkedin.com/in/jogo-media-2541501b5/" target="_blank"><LinkedIn /></a>{" "}
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="sec-right-wrap mobile-icon-links">
-                      <div
-                        style={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        <div className="left hide">
-                          {" "}
-                          <Link to={"/pre-sale"}><Presale classes={"sale-btn"} /></Link>
-                        </div>
-                        <div className="right"></div>
-                        <ul
-                          className="social-icons"
-                          style={{
-                            display: "flex",
-                            listStyle: "none",
-                            gap: "15px",
-                          }}
-                        >
-                          <li>
-                            <Link onClick={openPdfInNewTab}>
-                              {" "}
-                              <Doc />
-                            </Link>
-                          </li>
-                          <li>
-                            <a
-                              href="https://twitter.com/JogoAimeme"
-                              target="_blank"
-                            >
-                              <Twiitter />
-                            </a>
-                          </li>
-                          <li>
-                            <a href="https://t.me/JogoAimeme" target="_blank">
-                              <Telegram />
-                            </a>{" "}
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-12 hide-on-mobile">
-                  <div className="bottom-wrap">
-                    <div className="left-side">
-                      <Header
-                        state={state}
-                        setState={setState}
-                        activeSection={activeSection}
-                        setActiveSection={setActiveSection}
-                      />
-                    </div>
-                    <div className="right-side">
-                      <div className="main-screen">
-                        <section className="home-section-one" id="home">
-                          <div className="home-wrap">
-                            <div className="row">
-                              <div className="col-lg-6">
-                                <h2 className="first-sec-title">
-                                  Join the AI meme revolution
-                                  <br />
-                                  with AIM!{" "}
-                                </h2>
-                                <div className="detail">
-                                  <p>
-                                    Welcome to AIM - the future of AI personal agents! Infusing cutting-edge AI with the humor of memes, we're pioneering an engaging digital experience. Join us, invest in innovation, and be part of this game-changing platform poised for industry leadership.
-                                  </p>
-                                </div>
-                                <div className="white-paper">
-                                  <Link onClick={openPdfInNewTab}>
-                                  <WhitePaper/>
-                                  </Link>
-                                </div>
-                              </div>
-                              <div className="col-lg-6"></div>
-                            </div>
-                            <img
-                              className="image-one"
-                              src="/assets/images/section-one/sec-1.png"
-                              alt=""
-                            />
-                            <img
-                              className="image-two"
-                              src="/assets/images/section-one/sec-2.png"
-                              alt=""
-                            />
-                            <img
-                              className="image-three"
-                              src="/assets/images/section-one/sec-3.png"
-                              alt=""
-                            />
-                            <img
-                              className="image-four"
-                              src="/assets/images/section-one/sec-4.png"
-                              alt=""
-                            />
-                            <img
-                              className="image-five"
-                              src="/assets/images/section-one/sec-5.png"
-                              alt=""
-                            />
-                            <img
-                              className="image-six"
-                              src="/assets/images/section-one/sec-6.png"
-                              alt=""
-                            />
+                        <div className="sec-right-wrap hide-on-mobile">
+                          <div className="left hide">
+                            <span onClick={changeNetwork}>
+                              <Wallet classes={"wallet-btn"} />
+                            </span>
                           </div>
-                        </section>
-                        <BillGates />
-                        <Video />
-                        <PreSales changeNetwork={changeNetwork} account={account} setAccount={setAccount} loader ={loader} setloader={setloader}/>
-                        <div className="sticky-background">
+                          <div className="right">
+                            <Link >
+                              <span onClick={() => setState('presale-section')}>
+                                <BuyNow activeSection={activeSection} />
+                              </span>
+                            </Link>
+                            <ul className="social-icons">
+                              <li>
+                                <Link onClick={() => { changeNetwork, setShowPopup(true) }}>
+                                  <Wallet />
+                                </Link>
+                              </li>
+                              {ShowPopup &&
+                                <div className="connect-wallet-popup">
+                                  <div className="connect-wallet-inner">
+                                    <div onClick={() => setShowPopup(false)} className="close-btn">
+                                      X
+                                    </div>
+                                    <div className="images-holder">
+                                      <div className="images-inner ">
+                                        <img src={metamask} alt="" className="img-70" />
+                                      </div>
+                                      <div className="images-inner">
+                                        <img src={trustwallet} className=" img-100" alt="" />
+                                      </div>
+
+                                    </div>
+                                  </div>
+                                </div>
+                              }
+                              <li>
+                                <a href="https://twitter.com/JogoAimeme" target="_blank"><Twiitter /></a>
+                              </li>
+                              <li>
+                                <a href="https://t.me/JogoAimeme" target="_blank"><Telegram /></a>{" "}
+                              </li>
+                              <li>
+                                <a href="https://www.linkedin.com/in/jogo-media-2541501b5/" target="_blank"><LinkedIn /></a>{" "}
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="sec-right-wrap mobile-icon-links">
                           <div
-                            className="bg-fixed-img"
-                            onClick={() => setClose("")}
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                            }}
                           >
-                            <img
-                              src="assets/images/sticky.png"
-                              alt=""
-                              width={"100%"}
-                              height={"100%"}
-                            />
-                          </div>
-                          <div className="sec-absolute">
-                            <TokenEconomics
-                              index={index}
-                              setIndex={setIndex}
-                              id="token"
-                              activeRound={roundNumber}
-                            />
-
-                            <FundAllocation index={index} setIndex={setIndex} />
-
-                            <Snack close={close} />
-
-                            <Team />
-
-                            <About />
-                            <FAQ />
+                            <div className="left hide">
+                              {" "}
+                              <Link to={"/pre-sale"}><Presale classes={"sale-btn"} /></Link>
+                            </div>
+                            <div className="right"></div>
+                            <ul
+                              className="social-icons"
+                              style={{
+                                display: "flex",
+                                listStyle: "none",
+                                gap: "15px",
+                              }}
+                            >
+                              <li>
+                                <Link onClick={openPdfInNewTab}>
+                                  {" "}
+                                  <Doc />
+                                </Link>
+                              </li>
+                              <li>
+                                <a
+                                  href="https://twitter.com/JogoAimeme"
+                                  target="_blank"
+                                >
+                                  <Twiitter />
+                                </a>
+                              </li>
+                              <li>
+                                <a href="https://t.me/JogoAimeme" target="_blank">
+                                  <Telegram />
+                                </a>{" "}
+                              </li>
+                            </ul>
                           </div>
                         </div>
                       </div>
                     </div>
+                    <div className="col-lg-12 hide-on-mobile">
+                      <div className="bottom-wrap">
+                        <div className="left-side">
+                          <Header
+                            state={state}
+                            setState={setState}
+                            activeSection={activeSection}
+                            setActiveSection={setActiveSection}
+                          />
+                        </div>
+                        <div className="right-side">
+                          <div className="main-screen">
+                            <section className="home-section-one" id="home">
+                              <div className="home-wrap">
+                                <div className="row">
+                                  <div className="col-lg-6">
+                                    <h2 className="first-sec-title">
+                                      Join the AI meme revolution
+                                      <br />
+                                      with AIM!{" "}
+                                    </h2>
+                                    <div className="detail">
+                                      <p>
+                                        Welcome to AIM - the future of AI personal agents! Infusing cutting-edge AI with the humor of memes, we're pioneering an engaging digital experience. Join us, invest in innovation, and be part of this game-changing platform poised for industry leadership.
+                                      </p>
+                                    </div>
+                                    <div className="white-paper">
+                                      <Link onClick={openPdfInNewTab}>
+                                        <WhitePaper />
+                                      </Link>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-6"></div>
+                                </div>
+                                <img
+                                  className="image-one"
+                                  src="/assets/images/section-one/sec-1.png"
+                                  alt=""
+                                />
+                                <img
+                                  className="image-two"
+                                  src="/assets/images/section-one/sec-2.png"
+                                  alt=""
+                                />
+                                <img
+                                  className="image-three"
+                                  src="/assets/images/section-one/sec-3.png"
+                                  alt=""
+                                />
+                                <img
+                                  className="image-four"
+                                  src="/assets/images/section-one/sec-4.png"
+                                  alt=""
+                                />
+                                <img
+                                  className="image-five"
+                                  src="/assets/images/section-one/sec-5.png"
+                                  alt=""
+                                />
+                                <img
+                                  className="image-six"
+                                  src="/assets/images/section-one/sec-6.png"
+                                  alt=""
+                                />
+                              </div>
+                            </section>
+                            <BillGates />
+                            <Video />
+                            <PreSales changeNetwork={changeNetwork} account={account} setAccount={setAccount} loader={loader} setloader={setloader} />
+                            <div className="sticky-background">
+                              <div
+                                className="bg-fixed-img"
+                                onClick={() => setClose("")}
+                              >
+                                <img
+                                  src="assets/images/sticky.png"
+                                  alt=""
+                                  width={"100%"}
+                                  height={"100%"}
+                                />
+                              </div>
+                              <div className="sec-absolute">
+                                <TokenEconomics
+                                  index={index}
+                                  setIndex={setIndex}
+                                  id="token"
+                                  activeRound={roundNumber}
+                                />
+
+                                <FundAllocation index={index} setIndex={setIndex} />
+
+                                <Snack close={close} />
+
+                                <Team />
+
+                                <About />
+                                <FAQ />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <MobileLayout />
                   </div>
                 </div>
-                <MobileLayout />
               </div>
             </div>
           </div>
-        </div>
-      </div>
       }
     </>
   );
